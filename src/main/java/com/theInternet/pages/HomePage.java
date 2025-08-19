e2c0fab324153456ee4617ec42706581c687e595
“KseniiaSahakyan” <byboch@yandex.ru>
2025-08-19 17:27:01 +0200

    Initial commit

---
+++ src/main/java/com/theInternet/pages/HomePage.java
@@ -0,0 +1,11 @@
+package com.theInternet.pages;
+
+import com.theInternet.core.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+}
